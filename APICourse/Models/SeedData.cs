cd0d41c9d06f76cb9eda7343b2e51486b923631f
ChrisChrisLoLo <sporewoh@gmail.com>
2018-08-25 23:23:44 -0600

    Removed extraneous file, added implemented SeedDate method, but have yet to test it.

--- APICourse/Models/SeedData.cs
+++ APICourse/Models/SeedData.cs
@@ -11,11 +11,25 @@ namespace APICourse.Models
 {
     public class SeedData
     {
-        //private readonly IHostingEnvironment _hostingEnvironment;
+        /*
+        private static T GetOrCreate<T>(APICourseContext context,string[] queryData){
+            try
+            {
+                var queryObject = context.Courses;
+                return (T)Convert.ChangeType(queryObject,typeof(T));
+            }
+
+            catch
+            {
+                return ;
+            }
+                
+        }
+        */
 
         public static void Initialize(IServiceProvider serviceProvider)
         {
-            using (var context = new APICourseContext(
+            using (APICourseContext context = new APICourseContext(
                 serviceProvider.GetRequiredService<DbContextOptions<APICourseContext>>()))
             {
                 // Look for any movies.
@@ -23,15 +37,67 @@ namespace APICourse.Models
                 {
                     return;   // DB has been seeded
                 }
-                var file = Path.Combine(Directory.GetCurrentDirectory(),"Models","SeedDataInput","UAlbertaCoursesSingleTable.csv");
 
-                using (var reader = new StreamReader(file))
+                string file = Path.Combine(Directory.GetCurrentDirectory(),"Models","SeedDataInput","UAlbertaCoursesSingleTable.csv");
+
+                using (StreamReader reader = new StreamReader(file))
                 {
-                    Console.WriteLine("SDFFSDDFSFDSFDFSD");
+                    while (!reader.EndOfStream)
+                    {
+                        string csvRow = reader.ReadLine();
+                        string[] csvRowData = csvRow.Split(",");
+
+                        Faculty queryFaculty;
+                        Subject querySubject;
+
+                        string csvFacultyName = csvRowData[0];
+                        string csvSubjectName = csvRowData[1];
+                        string csvSubjectLetters = csvRowData[2];
+                        string csvCourseNumbers = csvRowData[3];
+                        string csvCourseTitle = csvRowData[4];
+                        string csvCourseSummary = csvRowData[5];
+
+                        //Csv rows contain duplicate faculty and subject entries. Only add entries to DB if they are unique.
+                        try
+                        {   
+                            //Assume entry already exists in DB.
+                            queryFaculty = context.Faculties.Single(f => f.Name == csvFacultyName);
+                        }
+                        catch
+                        {
+                            Faculty newFaculty = new Faculty { Name = csvFacultyName };
+                            context.Faculties.Add(newFaculty);
+                            context.SaveChanges();
+                            //Querying after adding newFaculty since the queried faculty should now have an id generated after saving.
+                            queryFaculty = context.Faculties.Single(f => f.Name == csvFacultyName);
+
+                        }
+
+                        try
+                        {
+                            querySubject = context.Subjects.Single(s => s.Name == csvSubjectName && s.LetterCode == csvSubjectLetters);
+                        }
+                        catch
+                        {
+                            Subject newSubject = new Subject { Name = csvSubjectName,
+                                                               LetterCode = csvSubjectLetters,
+                                                               Faculty = queryFaculty};
+                            context.Subjects.Add(newSubject);
+                            context.SaveChanges();
+                            querySubject = context.Subjects.Single(s => s.Name == csvSubjectName && s.LetterCode == csvSubjectLetters);
+                        }
+
+                        Course newCourse = new Course { NumberCode = Int32.Parse(csvCourseNumbers),
+                                                        Name = csvCourseTitle,
+                                                        Description = csvCourseSummary,
+                                                        Subject = querySubject};
+                        context.Courses.Add(newCourse);
+                        context.SaveChanges();
+                    }
                 }
 
 
-                context.SaveChanges();
+                //context.SaveChanges();
             }
         }
     }
